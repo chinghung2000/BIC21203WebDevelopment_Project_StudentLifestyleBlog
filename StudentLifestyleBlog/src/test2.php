ae6754da7b99338f4d999cb1763e2a46c771353c
Ching Hung Tan <103587066+chinghung2000@users.noreply.github.com>
2022-12-06 16:30:25 +0800

    Add files via upload

--- StudentLifestyleBlog/src/test2.php
+++ StudentLifestyleBlog/src/test2.php
@@ -1,12 +1,13 @@
 <?php
 
 declare(strict_types=1);
+require_once "Admin.php";
 require_once "User.php";
 
-// $admin = (new Admin())->login(1, "1234");
-// if ($admin) echo $admin->getId();
+$admin = (new Admin())->login(1, "admin");
+if ($admin) echo $admin->getName();
 
-$user = (new User())->login("asd", "asd");
-if ($user) echo $user->getId();
+// $user = (new User())->login("asd", "asd");
+// if ($user) echo $user->getId();
 
 ?>
